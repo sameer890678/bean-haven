7acbe2f8b023f94b4d8952e9ad19a166b5d97280
sameer_first <maliksameer890678@gmail.com>
2026-07-20 17:40:43 +0500

    removed about us properly

--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@ import Image from "next/image";
 import Navbar from "./component/Navbar";
 import Hero from "./component/Hero";
 import FeaturedMenu from "./component/FeaturedMenu";
-import AboutUs from "./component/AboutUs";
 import About from "./component/About";
 import Gallery from "./component/Gallery";
 
