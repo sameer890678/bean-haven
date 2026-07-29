984e7cd1b111b569a7d8434b845ac6690790e594
sameer_first <maliksameer890678@gmail.com>
2026-07-29 15:25:35 +0500

    making footer work

--- app/component/Footer.jsx
+++ app/component/Footer.jsx
@@ -38,31 +38,31 @@ export default function Footer() {
           <ul className="space-y-3">
 
             <li>
-              <Link href="/" className="hover:text-amber-400 transition">
+              <Link href="#home" className="hover:text-amber-400 transition">
                 Home
               </Link>
             </li>
 
             <li>
-              <Link href="/menu" className="hover:text-amber-400 transition">
+              <Link href="#menu" className="hover:text-amber-400 transition">
                 Menu
               </Link>
             </li>
 
             <li>
-              <Link href="/gallery" className="hover:text-amber-400 transition">
+              <Link href="#gallery" className="hover:text-amber-400 transition">
                 Gallery
               </Link>
             </li>
 
             <li>
-              <Link href="/about" className="hover:text-amber-400 transition">
+              <Link href="#about" className="hover:text-amber-400 transition">
                 About
               </Link>
             </li>
 
             <li>
-              <Link href="/contact" className="hover:text-amber-400 transition">
+              <Link href="#contact" className="hover:text-amber-400 transition">
                 Contact
               </Link>
             </li>
