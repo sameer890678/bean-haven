aac7be6a9cb50c6da86498caff10984ae01ab30c
sameer_first <maliksameer890678@gmail.com>
2026-07-29 20:10:43 +0500

    fixing bug for AOS

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import "aos/dist/aos.css";
-import AOSProvider from "./components/AOSProvider";
+import AOSProvider from "./component/AOSProvider";
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col"><AOSProvider>{children}</AOSProvider></body>
     </html>
   );
 }
