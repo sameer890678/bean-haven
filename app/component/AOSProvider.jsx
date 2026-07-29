ffe0b8e29dba54f3974a3ceef982e7d5d3efa4e1
sameer_first <maliksameer890678@gmail.com>
2026-07-29 20:35:57 +0500

    trying to make AOS work

--- app/component/AOSProvider.jsx
+++ app/component/AOSProvider.jsx
@@ -1,20 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
-import AOS from "aos";
-
 export default function AOSProvider({ children }) {
-  useEffect(() => {
-    console.log("AOS initialized");
-
-    AOS.init({
-      duration: 900,
-      once: true,
-      easing: "ease-in-out",
-    });
-
-    AOS.refresh();
-  }, []);
-
+  alert("AOS Provider Loaded");
   return <>{children}</>;
 }
